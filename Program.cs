ffc63891ad60956a29b75b7671d8773a6631c27d
noah the goodra <peterpan0413@live.com>
2021-06-12 01:46:10 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BsaFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
